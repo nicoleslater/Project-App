177cd50ddc0ebe2738fc4ce15b8e73d342814aa8
Nicole Slater <nicoleslater@pursuit.org>
2023-06-08 13:18:59 -0400

    Smooth Sailing

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ First Project Of Module 2
 
 I would like to create an application for children if they need to place an order for their parent its easy for them to use. I have installed the nanoid and chalk because I believe children are fascinateby bright colors. They also have the inclination to want to help mom or dad when it comes to a mobile device, laptop etc. 
 
+// The JSON.stringify() static method converts a JavaScript value to a JSON string, optionally replacing values if a replacer function is specified or optionally including only the specified properties if a replacer array is specified.
--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const {writeJSONFile, readJSONFile} = require("./helpers");
+const {faker} = require("@faker-js/faker");
 const log = console.log
 
 
-let customers = readJSONFile("")
+
 
 function run(){
+const action = process.argv[2];
+const customer = process.argv[3];
+switch(action){
+    case "create":
+        log(action, customer);
+        break;
+    case "read":
+        log(action, customer);
+        break;
+    case "update":
+        log(action, customer);
+        break;
+    case "destroy":
+        log(action, customer);
+        break;
+    default:
+        log("There is no customer by that name.");
 
+}
 }
 
 run()
