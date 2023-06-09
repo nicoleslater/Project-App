8054768cbc337a74b7aa9351936b318524bcc452
Nicole Slater <nicoleslater@pursuit.org>
2023-06-09 18:11:09 -0400

    Creating Customers

--- helpers.js
+++ helpers.js
@@ -11,7 +11,11 @@ function readJSONFile(path, fileName){
 }
 
 function writeJSONFile(path, fileName, data){
-    //  
+    // The JSON.stringify() static method 
+    // converts a JavaScript value to a JSON string
+    // optionally replacing values if a replacer function
+    // s specified or optionally including
+    // only the specified properties if a replacer array is specified.
     const data = JSON.stringify(data);
     // take the data make it JSON and put it in the file 
     return writeFileSync(`${path}/${fileName}`, data, {encoding: "utf-8"})
--- src/data/customers.js
+++ src/data/customers.js
@@ -1,3 +1,42 @@
-const customers = {
- 
-};
+const customers = [
+ {
+    name: "Paul",
+    product: "Shrimp Basket",
+    priceInCents: 599,
+    inStock: true
+ },
+ {
+    name: "Jaiden",
+    product: "Cheesburger Basket",
+    priceInCents: 699,
+    inStock: true
+
+ },
+ {
+    name: "Simone",
+    product: "Salmon Basket",
+    priceInCents: 799,
+    inStock: true
+ },
+ {
+    name: "McKenzie",
+    product: "BBQ Chicken Basket",
+    priceInCents: 899,
+    inStock: true
+ },
+ {
+    name: "Logan",
+    product: "Macaroni and Cheese Basket",
+    priceInCents: 799,
+    inStock: true
+ },
+ {
+    name: "Tashea",
+    pronunciation: "Ta-Sha",
+    products: "Grilled Chicken Basket",
+    priceInCents: 599,
+    inStock: true 
+
+ },
+];
+module.exports = customers;
