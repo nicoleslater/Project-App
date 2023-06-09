892829dd07990e079e920ba5dd78a23f95fc8a28
Nicole Slater <nicoleslater@pursuit.org>
2023-06-09 18:24:20 -0400

    Maintaining Objects

--- src/data/customers.js
+++ src/data/customers.js
@@ -2,15 +2,19 @@ const customers = [
  {
     name: "Paul",
     product: "Shrimp and Steak Basket",
+    cookedMeat: "Medium Well Done",
     priceInCents: 599,
     inStock: true,
+    specialRequests: false
     
  },
  {
     name: "Jaiden",
     product: "Cheesburger and French Fries Basket",
+    cookedMeat: "Well Done",
     priceInCents: 699,
     inStock: true,
+    specialRequests: false
     
 
  },
@@ -19,6 +23,7 @@ const customers = [
     product: "Salmon and Yellow Rice Basket",
     priceInCents: 799,
     inStock: true,
+    specialRequests: false
     
  },
  {
@@ -26,11 +31,13 @@ const customers = [
     product: "BBQ Chicken with Macaroni and Cheese Basket",
     priceInCents: 899,
     inStock: true,
+    specialRequests: false
     
  },
  {
     name: "Logan",
     product: "Personal Pizza",
+    specialRequests: "Cut into 4 slices",
     topping: "Cheese",
     sauce: "Tomato",
     priceInCents: 799,
@@ -40,7 +47,8 @@ const customers = [
  {
     name: "Tashea",
     pronunciation: "Ta-Sha",
-    products: "Grilled Chicken Basket",
+    products: "Grilled Chicken and Lettuce Basket",
+    specialRequests: "Please make sure the chicken is in bite size",
     priceInCents: 599,
     inStock: true,
    
