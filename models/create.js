22ebb768c4b2f3ff9254ecf47dca7c7ea03cdb74
Nicole Slater <nicoleslater@pursuit.org>
2023-06-11 14:46:27 -0400

    Fixing functions

--- models/create.js
+++ models/create.js
@@ -1,11 +1,10 @@
-
 const chalk = require("chalk");
 const log = console.log
 
 
 function createCustomerProduct(products){
     let customerPurchase = 0
-    for (let product of products){
+    for (let _product of products){
         return customerPurchase;
     }
     log(chalk.green("Thank you for submitting your order"))  
